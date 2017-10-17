d6f63d68a8e9a351d2543d245ff9da1979aa31b2
FIC <a.bmoraes@outlook.com>
2017-10-17 18:35:08 -0200

    Exemplo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExemploWhile
+{
+    class Program
+    {
+        static double salario = 100;
+
+        static void Main(string[] args)
+        {
+            while(salario<5000) {
+                Console.WriteLine(salario=salario*100);
+            }   
+        }
+    }
+}
